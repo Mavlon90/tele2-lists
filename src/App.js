e8c3882ecb299ae9a1455fcbec9dd3c11871bf24
Mavlon <Mavlon90@users.noreply.github.com>
2020-02-05 12:00:20 +0500

    Some changed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import TariffsList from './componets/TariffsList/TariffsList';
-import Tariff from './componets/Tariffs/Tariff'
 
 function App() {
   return (
