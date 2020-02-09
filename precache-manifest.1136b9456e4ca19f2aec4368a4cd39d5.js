1a1ce22e3b9c34078ca2c47bbbff251f321e0073
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-09 10:01:10 +0000

    Deploy Mavlon90/tele2-lists to github.com/Mavlon90/tele2-lists.git:gh-pages

---
+++ precache-manifest.1136b9456e4ca19f2aec4368a4cd39d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86a429f225ce6661b4f56a086b392554",
+    "url": "/tele2-lists/index.html"
+  },
+  {
+    "revision": "3952f375253da38fdca7",
+    "url": "/tele2-lists/static/css/main.074a5265.chunk.css"
+  },
+  {
+    "revision": "9953b9fb7dd60cf614ae",
+    "url": "/tele2-lists/static/js/2.0839c24a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tele2-lists/static/js/2.0839c24a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3952f375253da38fdca7",
+    "url": "/tele2-lists/static/js/main.c39bef00.chunk.js"
+  },
+  {
+    "revision": "403708bea1e23d0a8801",
+    "url": "/tele2-lists/static/js/runtime-main.844666ce.js"
+  }
+]);
